683529b906a28387741d49e5dcfc2a26d5e48401
varun <varun.puppala@terabee.com>
2023-12-25 23:06:02 +0100

    add cmake

--- base64_src/src/base64.c
+++ base64_src/src/base64.c
@@ -1,6 +1,5 @@
+#include "base64.h"
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdint.h>
 #include <string.h>
 
 const char base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
@@ -9,22 +8,6 @@ bool base64_encode(uint8_t *input, char *output, uint8_t input_size, uint8_t out
 bool base64_decode(char *input, uint8_t *output, uint8_t input_size, uint8_t output_size);
 
 static bool base64_char_position(char input_char, uint8_t *position);
-
-int main() {
-    char output_buf[12] = { 0 };
-    char *input_1 = "GFbrAavN7w==";
-    uint8_t output_buf_1[7] = { 0 };
-    char *input_2 = "GFY=";
-    uint8_t single_buf[2] = { 0 };
-
-    base64_decode(input_1, output_buf_1, strlen(input_1), sizeof(output_buf_1));
-    printf("%02X%02X%02X%02X%02X%02X%02X\n", output_buf_1[0],output_buf_1[1],output_buf_1[2], output_buf_1[3],output_buf_1[4],output_buf_1[5],output_buf_1[6]);
-    base64_encode(output_buf_1, output_buf, sizeof(output_buf_1), sizeof(output_buf));
-    printf("%s\n", output_buf);
-
-    base64_decode(input_2, single_buf, strlen(input_2), sizeof(single_buf));
-    printf("%02X%02X\n", single_buf[0], single_buf[1]);
-}
 
 bool base64_encode(uint8_t *input, char *output, uint8_t input_size, uint8_t output_size) {
 
