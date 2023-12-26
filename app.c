08d48cf9e13110d993e4a8b2b6f18881b9cd5f45
varun <varun.puppala@terabee.com>
2023-12-26 22:24:19 +0100

    add aes library

--- app.c
+++ app.c
@@ -1,19 +1,41 @@
+#define CBC 1
+
 #include "base64.h"
+#include "aes.h"
 #include <string.h>
 #include <stdio.h>
 
+char *iv = "ThisistheIV16byt";
+char *key = "Thisisthekey16Bt";
+
+static void print_array(uint8_t *array, uint8_t size);
+
 int main() {
-    char output_buf[12] = { 0 };
-    char *input_1 = "GFbrAavN7w==";
-    uint8_t output_buf_1[7] = { 0 };
-    char *input_2 = "GFY=";
-    uint8_t single_buf[2] = { 0 };
+    uint8_t sample_array[16] = { 0 };
+    struct AES_ctx ctx;
 
-    base64_decode(input_1, output_buf_1, strlen(input_1), sizeof(output_buf_1));
-    printf("%02X%02X%02X%02X%02X%02X%02X\n", output_buf_1[0],output_buf_1[1],output_buf_1[2], output_buf_1[3],output_buf_1[4],output_buf_1[5],output_buf_1[6]);
-    base64_encode(output_buf_1, output_buf, sizeof(output_buf_1), sizeof(output_buf));
-    printf("%s\n", output_buf);
+    for(int i = 0; i < sizeof(sample_array); i++) {
+        sample_array[i] = i;
+    }
 
-    base64_decode(input_2, single_buf, strlen(input_2), sizeof(single_buf));
-    printf("%02X%02X\n", single_buf[0], single_buf[1]);
+    //base64 encode the array
+    uint8_t encoded_array[24] = { 0 };
+    AES_init_ctx_iv(&ctx, key, iv);
+    AES_CBC_encrypt_buffer(&ctx, sample_array, sizeof(sample_array));
+    print_array(sample_array, sizeof(sample_array));
+    base64_encode(sample_array, encoded_array, sizeof(sample_array), sizeof(encoded_array));
+    print_array(encoded_array, sizeof(encoded_array));
+
+    //decrypt
+    base64_decode(encoded_array, sample_array, sizeof(encoded_array), sizeof(sample_array));
+    AES_ctx_set_iv(&ctx, iv);
+    AES_CBC_decrypt_buffer(&ctx, sample_array, sizeof(sample_array));
+    print_array(sample_array, sizeof(sample_array));
+}
+
+static void print_array(uint8_t *array, uint8_t size){
+    for(int i = 0; i < size; i++) {
+        printf("%02X ", array[i]);
+    }
+    printf("\n");
 }
