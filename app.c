20c40a982eae27b291c60abbf18917cf3ac909c7
varun <venkata.varun.puppala@trincebio.com>
2024-01-25 09:41:03 +0000

    fix table encryption logic

--- app.c
+++ app.c
@@ -36,46 +36,46 @@ int main() {
     encrypt_vial(&ctx, &v1);
     encode_vial(&v1,encoded_array, sizeof(encoded_array));
     print_base64_chars(encoded_array, sizeof(encoded_array));
-    print_vial(&v1);
+    //print_vial(&v1);
 
     //decrypt
     base64_decode(encoded_array, (uint8_t *)&v1, sizeof(encoded_array), sizeof(v1));
     decrypt_vial(&ctx1, &v1);
-    print_vial(&v1);
+    //print_vial(&v1);
 
-    //table stuff
-    token_table_t table = { 0 };
-    token_table_t table_1 = { 0 };
-    set_table(&table);
+    // //table stuff
+    // token_table_t table = { 0 };
+    // token_table_t table_1 = { 0 };
+    // set_table(&table);
 
-    //need a buffer of 1088 bytes for an input of 816 bytes
-    //TODO: add table encryption logic
-    char encoded_table[1088] = { 0 };
+    // //need a buffer of 1088 bytes for an input of 816 bytes
+    // //TODO: add table encryption logic
+    // char encoded_table[1088] = { 0 };
 
-    //try to use the same context
-    encrypt_table(&ctx, &table, sizeof(table));
-    encode_table(&table, encoded_table, sizeof(encoded_table));
+    // //try to use the same context
+    // encrypt_table(&ctx, &table, sizeof(table));
+    // encode_table(&table, encoded_table, sizeof(encoded_table));
 
-    print_base64_chars(encoded_table, sizeof(encoded_table));
+    // print_base64_chars(encoded_table, sizeof(encoded_table));
 
-    decode_table(encoded_table, &table_1, sizeof(encoded_table), sizeof(table_1));
-    decrypt_table(&ctx, &table_1, sizeof(table_1));
+    // decode_table(encoded_table, &table_1, sizeof(encoded_table), sizeof(table_1));
+    // decrypt_table(&ctx, &table_1, sizeof(table_1));
 
-    printf("%X\n", table_1.rows[49].original_tokens);
-    printf("%lX\n", table_1.table_version);
+    //printf("%X\n", table_1.rows[49].original_tokens);
+    //printf("%lX\n", table_1.table_version);
 
     //try the output of the machine
     char *input_machine_answer = "YlsEUJQ+gLVCHEwQkVuZtQ==";
 
-    char *input_table = "a4440r2NEtxqyUv/Kb1B+rwTvVE1MM6/aVQz3aIBmN6CyL8iQi1L7u/z88Ycg3MjvQMBcAnBSEgtaXq6nd7+MNGyYUGbbos7ryxxc6Jtmyj60MlCgqNaBf9NmeBN+FdpQ4A98LI9hkRuMHEw/lXpKbSfG2qrLtzCl73kjAXeJKIUjQRsDwrrPlYbFBs7K+tfrVU5rdvMgZprVlqR/fzgQccouwsk2IFyCBFTUy6UH02PuJuLnCs2FJ+RQ9ijUCk7jGADd5+cn5jLJ3lEoXrPDXaEPcNZgrbt7Sle8jy5u7ubg8jvNX+lwAa6p+OWdfDepvUvWxNZtdKIyjKL6b89tIxcVRgKe0+gQ4iVkWerxHQ8DZ6DtzZ8pbEwWzSx9CNMMwcl2nXwIC6ErpoCjiqvL7RY/bKOnWvpo8cIALNSMENYmrFpzgcIslenoXu48g9vTDJqTS7jjSaJ+NMCGj8Z/Uv6svHV3DGD7rEOJ/l2YmiBk1DEMuMlPJ2E4N76mlsr4ieP8XcG2idkesg3zA9kZL46u/lEXNpKNnXQGNMGQN5Qi5dHfyA98ZmqpSidMyjNhByJM5oVZdm1dx9OHI6zTTuh7SqS9W29vT8HKYW2PlnIwXcCniOb1fMHMR1kCw8xKIDPqSB5+D+WrcHPudVp7osEAqncNTTHpUrpL9969zJvvx2Ui2Ra0orFuD8v5mzaqnHbUs77/1H8JoGPC8TFytx8ZOYVNdrOVZBM4Hd3CE5imA27x0xlva9fVG6t4qfrWixHf3+WQa3YyOOtLH6o/TBEnTDjHFDzP0TL38m5ANVPFXrf2muGY7rJT+hx1cEPbgosw6bdDTcH1S8Ob9efExN+MJFivKia+BBecYGkmedTjQbkdUDw1zEYLyT+HGCm/9FSsDT/tU/nsv7NJG6tYYEXLu1hQhZv0B3z1VepC2mP8+DEwfax3+78grjs0SCmf4y+mb213oS+8/KIyHEVDe1d9ByNW+Gm4mBst0kuzjcYLuE8ToKsXelkuPGxQ07qEtC1pHAx95q4cN/g1fdnSs7P3+MufGG9hcC1CRlb6FwMvOmjTYyBN/oOUll22ScO";
+    char *input_table = "sjUoT16DLPQnrCqJoYsESfy9xoATovcBCmHMS7fd1pGLp1eiZpF1UMvqk2srQIKmESgGGd1EHXAf+ZVuVHgdjb9VdV2QRfsIMnFZNSrH2gMii4XHUSX0s6Bx/In93hMwlflhEIX8ZKD8ymzEo2xzwHHL7fd6XJncIBRQUkBj+lsiW45kctP+c6m8Woxp+Mr1Skyz7pESmyQXKNcC8W0uvfz+YP88wcXgP7y6hc7mJ+iCGKD2uk2DI6ObatCDL5dCMBdsx9gu7swNhz2QYLhomzC74C/zpqvno6kF1qpx7VeVM0C76bYFvHu9ioeVyPhbs3N1e0fGflOlQQvZ7pezHPrdWWKRMzrwrBk+FmCwk/dTB5d0EHcdHOFCReitF73Om03RTWcCXbiQ8+tFhlanwWULMpcF60OLdONyr1gtf4ZdKStYl6IB5uUqmJWiw4k/KjoDOknov0nU32tTvhfKO32t709kYm5gj9nq2UtEGFGSSwtkmQY5cS5D/3ef++I6JV9GdS3yDOdFNHsoTo+jF0hdWPC3RU1TOl3jTRebxWX6TtTBA1SFcuXCHI8Jn5nePI7zHSOE9LKgauJg+PHtDBhdxI3CBpFQUpUMi7jH22bsbj1H6k/o3wwi4y2ggpk7pIkCDlqNcL5I1MgAfprRN6Zq988WcwJDlNhwzIiB5ZUyiv8oI24QovypBBfaYOlKNKn6SJd9wo3YFnMpfGvyIOXAeY1gnqapvX2wW4hkPr0dVYug9riNFdoVhADLlFyeC2/DF93b6wy0hujWOIdQYRAgd1mxJVMq8wzGP2Vx9wXrgLmTgoNIE3kji+7lpilKjjJsxbOQtMcvj+9bnNTsLytScYyIHePlkkzXSFqmilSXvEMse4nuAyaf5jjgxelQM7oK01duSn0aPSh8j+o1VEwFZma3NXuqT3t0RwIBvV4PqZORqa1XWxpro76C4hAWUmxXRF5zu+eaXouXcZKF9Rt6nCrlxefav5oGCV4Zi824ft8+QGsixfVcDM6zktuHNWx3KZ/QYWZKi/xhYWc0PPDIswJpguqswHjq4kIPIY66z1WJp9Ck1UpT3C0t/FG7";
 
     //base64 decode the vial first
-    vial_dets_t new_vial = { 0 };
+    // vial_dets_t new_vial = { 0 };
 
-    decode_vial(input_machine_answer, &new_vial, strlen(input_machine_answer));
+    // decode_vial(input_machine_answer, &new_vial, strlen(input_machine_answer));
 
-    decrypt_vial(&ctx2, &new_vial);
-    print_vial(&new_vial);
+    // decrypt_vial(&ctx2, &new_vial);
+    //print_vial(&new_vial);
 
     //decode the table now 
     token_table_t new_table = { 0 };
@@ -84,15 +84,23 @@ int main() {
     //decrypt table
     decrypt_table(&ctx3, &new_table, sizeof(new_table));
 
+    //print fields in the table 
+    printf("the version is %ld\n", new_table.table_version);
+    printf("the padding bytes are %ld\n", new_table.padding_bytes);
+    printf("the serial number is %02X\n", new_table.rows[0].serial_number);
+    printf("the tokens remaining are %X\n", new_table.rows[0].tokens_remaining);
+    printf("the tokens on vial are %X\n", new_table.rows[0].tokens_on_vial);
+    printf("the original tokens are %X\n", new_table.rows[0].tokens_on_vial);
+
     //to test the set table logic now
     // iterate every 24 base64 characters, which is 16 bytes
     uint8_t table_to_split[816] = { 0 };
     memcpy(table_to_split, &new_table, sizeof(table_to_split));
 
     char temp[24] = { 0 };
+    AES_ctx_set_iv(&ctx4,iv);
+    AES_init_ctx_iv(&ctx4, key, iv);
     for(uint8_t i = 0; i < 51; ++i) {
-        AES_ctx_set_iv(&ctx4,iv);
-        AES_init_ctx_iv(&ctx4, key, iv);
         AES_CBC_encrypt_buffer(&ctx4, &(table_to_split[16 * i]), 16);
         base64_encode(&(table_to_split[16 * i]), temp, 16, 24);
         print_string_array(temp, 24);
