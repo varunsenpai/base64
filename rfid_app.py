00a8e0e11e12fd09ee7df3d53e3fd855f89d03dd
varun <venkata.varun.puppala@trincebio.com>
2024-07-15 16:53:28 +0200

    add base64 decode logic

--- rfid_app.py
+++ rfid_app.py
@@ -1,4 +1,5 @@
 import ctypes
+import base64
 import sys
 
 so_file = "build/libbase64_app_lib.so"
@@ -13,16 +14,16 @@ class VialDets(ctypes.Structure):
         ("version_number", ctypes.c_uint8),
         ("serial_number", ctypes.c_uint32),
         ("token_count", ctypes.c_uint32),
-        ("original_topken_count", ctypes.c_uint32)
+        ("original_token_count", ctypes.c_uint32)
     ]
 
-def encode_vial_details(vial_details: VialDets):
+def encode_vial_details(vial_details: VialDets, version_number = 0, serial_number = 0, token_count = 0, original_token_count = 0) -> str:
 
     vial_details.magic_number = (ctypes.c_uint8 * 3)(0x18, 0x56, 0xEB)
-    vial_details.version_number = 1
-    vial_details.serial_number = 0xABCDEF02
-    vial_details.token_count = 0xFFFE
-    vial_details.original_token_count = 0xFFFD
+    vial_details.version_number = version_number
+    vial_details.serial_number = serial_number
+    vial_details.token_count = token_count
+    vial_details.original_token_count = original_token_count
 
     # Convert vial_details to bytes to match the expected input format for base64_encode
     input_data = bytearray(vial_details.magic_number) + bytearray([vial_details.version_number]) + \
@@ -44,13 +45,28 @@ def encode_vial_details(vial_details: VialDets):
     if success:
         # Convert output buffer to a Python string
         output_string = ctypes.string_at(output_buffer, output_size).decode('ascii')
-        print("Base64 Encoded Output:", output_string)
+        return output_string
     else:
         print("Base64 encoding failed")
 
+def decode_vial_details(encoded_string: str, vial: VialDets):
+    input_hex = base64.b64decode(encoded_string)
+    ctypes.memmove(ctypes.addressof(vial), input_hex, ctypes.sizeof(vial))
+
+def print_vial(vial: VialDets):
+    print("Magic Number:", ''.join(f"{x:02x}" for x in vial.magic_number))
+    print("Version Number:", f"{vial.version_number:02x}")
+    print("Serial Number:", f"{vial.serial_number:08x}")
+    print("Token Count:", f"{vial.token_count:08x}")
+    print("Original Token Count:", f"{vial.original_token_count:08x}")
+
+
 def main():
     input_vial = VialDets()
-    encode_vial_details(input_vial)
+    decoded_vial = VialDets()
+    output_string = encode_vial_details(input_vial, 1, 0xABCDEF02, 0xFFFE, 0xFFFD)
+    decode_vial_details(output_string, decoded_vial)
+    print_vial(decoded_vial)
 
 if __name__ == "__main__":
     main()
